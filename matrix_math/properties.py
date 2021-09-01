d55a7597cff355fd2fbe7dcb59f47bb5a3d0e8cb
Kelsi B <find.kelsi@gmail.com>
2021-08-31 22:48:01 -0700

    created test for transpose, out of range

--- matrix_math/properties.py
+++ matrix_math/properties.py
@@ -1,6 +1,15 @@
 def transpose(m):
     # columns become rows and rows become columns
-    m_t = m[:]
+    column_size = len(m[0])
+    row_size = len(m)
+    m_t = []
+    # make the temp matrix in appropriate size for result, np.zeros
+    for r in range(row_size):
+        temp = []
+        for c in range(column_size):
+            temp.append(0)
+        m_t.append(temp)
+
     for i, row in enumerate(m):
         for n, e in enumerate(row):
             m_t[n][i] = e
