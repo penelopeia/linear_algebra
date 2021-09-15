8f012cec12f7e980484f471fe131549803593111
Kelsi B <find.kelsi@gmail.com>
2021-09-14 22:49:20 -0700

    add placeholder for nargs optional arg params for specifying the matrix operator

--- calculator.py
+++ calculator.py
@@ -6,5 +6,11 @@ parser = argparse.ArgumentParser(description='Linear Algebra Operations.')
 parser.add_argument('--a_matrix', type=[], help='first matrix to operate on')
 parser.add_argument('--b_matrix', type=[], help='second matrix to operate on')
 
+# add calculator functions as args/options
+parser.add_argument('--dot', type=bool, help='set operation as dot product', nargs='?')
+
 args = parser.parse_args()
-print(simp.inner_product(args.a_matrix, args.b_matrix))
+if args.dot:
+    print(simp.inner_product(args.a_matrix, args.b_matrix))
+else:
+    print("specify operation with CLI param. See -h for details")
