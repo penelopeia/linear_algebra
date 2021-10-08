2fbdb039dc65838d74c284f273dd20a0860098c2
Kelsi B <find.kelsi@gmail.com>
2021-10-07 21:04:20 -0700

    simple add included to cli option

--- calculator.py
+++ calculator.py
@@ -8,9 +8,12 @@ parser.add_argument('--b_matrix', type=[], help='second matrix to operate on')
 
 # add calculator functions as args/options
 parser.add_argument('--dot', type=bool, help='set operation as dot product', nargs='?')
+parser.add_argument('--add', type=bool, help='set operation as addition', nargs='?')
 
 args = parser.parse_args()
 if args.dot:
     print(simp.inner_product(args.a_matrix, args.b_matrix))
+if args.add:
+    print(simp.add(args.a_matrix, args.b_matrix))
 else:
     print("specify operation with CLI param. See -h for details")
